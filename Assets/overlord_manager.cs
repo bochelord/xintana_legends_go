ff716e215ec6f213fcd370bbd83aa30838aeb8eb
bochelord <bochelord@gmail.com>
2016-01-30 00:28:18 +0100

    Added bakcground and mountains movement for first level

--- Assets/overlord_manager.cs
+++ Assets/overlord_manager.cs
@@ -1,14 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 using System;
 
-public class overlord_manager : MonoBehaviour {
+public class overlord_manager : MonoBehaviour
+{
 
+    #region variables
     public Transform explosion_big;
-    
+
+    public Transform cielo;
+    public Transform mountains;
+
+    #endregion variables
+
+
+
     // Use this for initialization
 	void Start () {
-        StartCoroutine(Kickme());
+        //StartCoroutine(Kickme());
+
+        //Intro_Sequence();
+
+        StartCoroutine(Intro_Sequence());
 	}
 	
 	// Update is called once per frame
@@ -23,16 +37,41 @@ public class overlord_manager : MonoBehaviour {
 
         for (int i = 0; i < 10; i++) {
 
-
-            float x_aleatoria = UnityEngine.Random.Range(-1f, 1f);
-            float y_aleatoria = UnityEngine.Random.Range(-1f, 1f);
+            float x_aleatoria = UnityEngine.Random.Range(-2f, 2f);
+            float y_aleatoria = UnityEngine.Random.Range(-2f, 2f);
 
             Transform tempobj = Instantiate(explosion_big, new Vector3(x_aleatoria, y_aleatoria, 0), Quaternion.identity) as Transform;
 
-            yield return new WaitForSeconds(0.2f);
+            yield return new WaitForSeconds(0.6f);
 
             tempobj.gameObject.SetActive(false);
         }
+
+    }
+
+
+    /// <summary>
+    /// Pues eso , la secuencia de la intro (se sube el cielo y se suben las montañas tambien
+    /// </summary>
+    IEnumerator Intro_Sequence()
+    {
+
+        // El cielo sube
+
+
+        cielo.DOMove(new Vector3(0,1.0f,0), 3f, false);
+
+        yield return new WaitForSeconds(1.5f);
+
+        //Las montañas suben...
+        mountains.DOMoveY(-1.6f, 1.7f);
+
+        // Aparece la pasarela
+
+        // Aparecen los dos enemigos
+
+        //etc...
+
 
     }
 }
