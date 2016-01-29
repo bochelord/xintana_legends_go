1191be0d47ee747b4f457a17b167124f363960b4
Leunam <leunam.san@gmail.com>
2016-01-29 22:49:37 +0100

    [Scene

---
+++ Assets/CombinationManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class CombinationManager : MonoBehaviour {
+
+    [Header("Links")]
+    public GameObject Canvas;
+    public Button prefabButton;
+
+    public int combinationLength;                   //Combination length that the User will have to solve.
+    public GameObject[] combinationArray;           //This is the Array with the GameObjects combination.
+    public GameObject[] objectsCombinationPool;     //Pool with all the possible GameObjects.
+    
+
+	// Use this for initialization
+	void Start () {
+        combinationArray = new GameObject[combinationLength];
+        GenerateCombination();
+        CreateButtonsAndPlaceThem();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void GenerateCombination() 
+    {
+        int tempValue = 0;
+        for (int i = 0; i < combinationLength; i++)
+        {
+            tempValue = Random.Range(0, objectsCombinationPool.Length);
+            combinationArray[i] = objectsCombinationPool[tempValue];
+        }
+    }
+    /// <summary>
+    /// Change the Combination Length.
+    /// </summary>
+    /// <param name="newValue"></param>
+    void ChangeCombinationLength(int newValue)
+    {
+        combinationLength = newValue;
+    }
+
+    /// <summary>
+    /// Instantiate the gameobjects and place them in the middle top of the screen.
+    /// </summary>
+    void CreateButtonsAndPlaceThem()
+    {
+        float offset = 0;
+        for (int i = 0; i < combinationLength; i++)
+        {
+            offset += 75;
+            Vector3 screenPosition = GetScreenPosition(offset);
+            GameObject buttonCloned = Instantiate(combinationArray[i]);
+            buttonCloned.transform.parent = Canvas.transform;
+            //buttonCloned.gameObject.transform.position = screenPosition;
+            
+        }
+    }
+
+    public Vector3 GetScreenPosition(float offset)
+    {
+
+        Vector3 screenPosition = new Vector3((Screen.width + offset)/2, Screen.height - 50, 0);
+        Debug.Log("ScreenPosition returned:" + screenPosition);
+        return screenPosition;
+    }
+}
