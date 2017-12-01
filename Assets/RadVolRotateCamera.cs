e53c8551a5a162e4f91c69f4cc8fb81ca809e69d
bochelord <bochelord@gmail.com>
2017-12-01 22:36:45 +0100

    [Scene + Package] Darkest Dungeon Combat camera effect recreated and scene setup with some scripts from 2DxFx package

---
+++ Assets/RadVolRotateCamera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class RadVolRotateCamera : MonoBehaviour {
+
+
+    public float angleCamera;
+    public float movementCamera;
+
+
+    //void Start()
+    //{
+    //    this.GetComponent<Camera>().orthographic = false;
+    //    this.transform.DORotate(new Vector3(0f,angleCamera,0f),2);
+    //    this.transform.DOMove(new Vector3(movementCamera, 1, -10), 2);
+    //}
+
+
+
+
+
+
+    public void Button_InvertCameraAngle()
+    {
+        this.GetComponent<Camera>().orthographic = false;
+        angleCamera = -angleCamera;
+        movementCamera = -movementCamera;
+        moveCamera();
+    }
+
+    void moveCamera()
+    {
+        this.transform.DORotate(new Vector3(0f, angleCamera, 0f), 2);
+        this.transform.DOMove(new Vector3(movementCamera, 1, -10), 2);
+    }
+
+    public void ResetCamera()
+    {
+        this.transform.position = new Vector3(0, 1, -10);
+        this.GetComponent<Camera>().orthographic = true;
+        this.transform.rotation = Quaternion.EulerAngles(0,0,0);
+    }
+}
