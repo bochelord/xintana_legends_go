269290a04ec8e554c2f36d94ffdcdc79767b2bbb
bochelord <bochelord@gmail.com>
2017-12-02 03:01:47 +0100

    [Scene + Code + Assets + animations] Added new scene intro and removed unused code

---
+++ Assets/RadLauncher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RadLauncher : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+        yield return new WaitForSeconds(12);
+        SceneManager.LoadScene(1);
+	}
+	
+	
+}
