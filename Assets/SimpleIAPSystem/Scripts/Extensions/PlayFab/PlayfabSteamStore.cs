47115286c13d38c37673a52e1501eb093e584668
bochelord <bochelord@gmail.com>
2017-12-07 15:57:30 +0100

    [Package] Added IAP system

---
+++ Assets/SimpleIAPSystem/Scripts/Extensions/PlayFab/PlayfabSteamStore.cs
@@ -0,0 +1,55 @@
+﻿/*  This file is part of the "Simple IAP System" project by Rebound Games.
+ *  You are only allowed to use these resources if you've bought them from the Unity Asset Store.
+ * 	You shall not license, sublicense, sell, resell, transfer, assign, distribute or
+ * 	otherwise make available to any third party the Service or the Content. */
+
+#if UNITY_PURCHASING && PLAYFAB_STEAM
+using UnityEngine;
+using UnityEngine.Purchasing.Extension;
+using PlayFab;
+using PlayFab.ClientModels;
+using Steamworks;
+
+namespace SIS
+{
+    /// <summary>
+    /// Represents the public interface of the underlying store system for the Steamworks API.
+    /// </summary>
+    public class PlayfabSteamStore : PlayfabStore
+    {
+        #pragma warning disable 0414
+        protected Callback<MicroTxnAuthorizationResponse_t> microTxnAuthorizationResponse;
+        #pragma warning restore 0414
+
+
+        public override void Initialize(IStoreCallback callback)
+        {
+            storeId = "Steam";
+            this.callback = callback;
+
+            microTxnAuthorizationResponse = Callback<MicroTxnAuthorizationResponse_t>.Create(OnMicroTxnAuthorizationResponse);
+        }
+
+
+		public override void OnPurchaseResult(PayForPurchaseResult result)
+		{
+			Debug.LogError("Status: " + result.Status + ", Currency: " + result.PurchaseCurrency +
+		        ", Price: " + result.PurchasePrice + ", ProviderData: " + result.ProviderData +
+		        ", PageURL: " + result.PurchaseConfirmationPageURL);
+		}
+
+
+        private void OnMicroTxnAuthorizationResponse(MicroTxnAuthorizationResponse_t pCallback)
+        {
+            Debug.LogError("OnMicroTxnAuthorizationResponse: " + pCallback.m_unAppID + ", " + pCallback.m_ulOrderID + ", " + pCallback.m_bAuthorized);
+
+			ConfirmPurchaseRequest request = new ConfirmPurchaseRequest()
+			{
+				OrderId = orderId
+			};
+
+			PlayFabClientAPI.ConfirmPurchase(request, OnPurchaseSucceeded, OnPurchaseFailed);
+        }
+    }
+}
+#endif
