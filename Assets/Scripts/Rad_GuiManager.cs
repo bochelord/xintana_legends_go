b276da5148479dfea16dd3b6c1ab36d4e7ef3c77
bochelord <bochelord@gmail.com>
2017-12-08 01:23:12 +0100

    [Scene] Added Lava and adjusted UI panels

--- Assets/Scripts/Rad_GuiManager.cs
+++ Assets/Scripts/Rad_GuiManager.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Rad_GuiManager : MonoBehaviour {
 
+    [Header("Panels")]
     public GameObject gmeOverPanel;
     public GameObject mainMenuPanel;
     public GameObject settingsPanel;
+
+    [Header("Text")]
+    public Text enemyText;
 
     /// <summary>
     /// called from Button, set main menu panel on and turns off the rest
