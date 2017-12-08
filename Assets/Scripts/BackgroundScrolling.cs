5b3594f8f1f572c8c76017a7bfe0e7151339dcab
bochelord <bochelord@gmail.com>
2017-12-08 17:52:08 +0100

    [Animations + scene +Code] Scrolling Background sky and animation from xintana reajusted

---
+++ Assets/Scripts/BackgroundScrolling.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScrolling : MonoBehaviour {
+
+    public Renderer mainRenderer;
+    public float speedMultiplier;          //The current speed multiplier
+    private Vector2 offset;
+
+    void Start()
+    {
+        //speedMultiplier = 1;
+        //paused = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //if (!paused)
+        //{
+            offset = mainRenderer.material.mainTextureOffset;
+            offset.x += speedMultiplier * Time.deltaTime;
+
+            if (offset.x > 1)
+                offset.x -= 1;
+
+            mainRenderer.material.mainTextureOffset = offset;
+        //}
+    }
+}
