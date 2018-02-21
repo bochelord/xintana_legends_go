9367733694039f7f390545e3a08713bc42166fe9
Alex <alex@radicalgraphics.com>
2018-02-21 11:31:00 +0100

    [Project] scripts in script folder + prizes list scriptable object

---
+++ Assets/Xintana Volcanic/Scripts/ScriptableObjects/Recipes/PrizesListScriptableObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum PrizeType { COINS,GEMS,TOKENS,WEAPON}
+[SerializeField]
+public class PrizesListScriptableObject : ScriptableObject {
+
+    [System.Serializable]
+    public class PrizeListClass
+    {
+        public string nameId;
+        public PrizeType categoryType;
+        public string name;
+        public string description;
+        public Sprite itemSprite;
+        public float itemValue;
+        public int prizePercentaje;
+
+    }
+
+    [Header(" Coins Items List")]
+    public List<PrizeListClass> coinsItemsList = new List<PrizeListClass>();
+    [Header(" Gems Items List")]
+    public List<PrizeListClass> gemsItemsList = new List<PrizeListClass>();
+    [Header(" Tokens Items List")]
+    public List<PrizeListClass> tokensItemsList = new List<PrizeListClass>();
+    [Header(" Weapons Items List")]
+    public List<PrizeListClass> weaponsItemsList = new List<PrizeListClass>();
+
+
+}
