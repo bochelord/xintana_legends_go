57f036be8cc25a4b46cb9b76f12f329f887f3f70
bochelord <bochelord@gmail.com>
2018-03-01 18:24:32 +0100

    [Scene] Updated credits with links to social networks,, still WIP

---
+++ Assets/LinksManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinksManager : MonoBehaviour {
+
+	public void Button_TwitterRadical()
+    {
+        Application.OpenURL("https://twitter.com/radicalgraph1cs");
+    }
+    public void Button_TwitterLeunamsan()
+    {
+        Application.OpenURL("https://twitter.com/leunamsan");
+    }
+    public void Button_Twitterbochelord()
+    {
+        Application.OpenURL("https://twitter.com/bochelord");
+    }
+}
+
