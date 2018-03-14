009147f7b4d17c6382fc28442f7ba043421054af
Manuel Gonzalez <leunam.san@gmail.com>
2018-03-14 16:42:24 +0100

    [Scene+Code] - Shop scene fixes to adjust shop elements to different resolutions.

---
+++ Assets/DynamicGrid.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DynamicGrid : MonoBehaviour {
+
+    public int col;
+
+    private RectTransform parent;
+    private GridLayoutGroup grid;
+	// Use this for initialization
+	void Start () {
+
+        parent = gameObject.GetComponent<RectTransform>();
+        grid = gameObject.GetComponent<GridLayoutGroup>();
+
+        grid.cellSize = new Vector2(parent.rect.width / col, grid.cellSize.y);
+    }
+
+    //private void Update()
+    //{
+    //    grid.cellSize = new Vector2(parent.rect.width / col, grid.cellSize.y);
+    //}
+
+}
