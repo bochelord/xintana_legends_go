f99af7dda0d013aef9a86972b47c6110656e1344
bochelord <bochelord@gmail.com>
2018-04-20 15:12:46 +0200

    [Scene + Code + Prefab] Added a back button to go back to menu from all the scene except the gameplay one. If back is touched on menu, a panel is shown to confirm quit... Also added localization for such panel

--- Assets/Rad_InputManager.cs
+++ Assets/Rad_InputManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rad_InputManager : MonoBehaviour {
 
@@ -48,9 +49,9 @@ public class Rad_InputManager : MonoBehaviour {
 
             }
 
-            if (currentSwipe.x > 0.25f || currentSwipe.y > 0.25f || currentSwipe.x < -0.25f || currentSwipe.y < -0.25f)
+            if (currentSwipe.x > 0.5f || currentSwipe.y > 0.5f || currentSwipe.x < -0.5f || currentSwipe.y < -0.5f)
             {
-                Instantiate(UISwipePrefab, Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0f, 0f, 10f), Quaternion.identity);
+                //Instantiate(UISwipePrefab, Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0f, 0f, 10f), Quaternion.identity);
             }
             else
             {
@@ -60,6 +61,34 @@ public class Rad_InputManager : MonoBehaviour {
                 
         }
 
+
+        //Back button quit -- Apparently the back button from Android is mapped in Unity to the "Escape" key
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            if (currentScene.name.Contains("Menu"))
+            {
+                //Application.Quit();
+                MainMenuManager mainMenuManager = FindObjectOfType<MainMenuManager>();
+                mainMenuManager.Show_QuitPanel();
+            }
+            else if (currentScene.name.Contains("Shop"))
+            {
+                AsyncLoadLevel.LoadScene("XintanaLegendsGo_Menu");
+            }
+            else if (currentScene.name.Contains("Settings"))
+            {
+                AsyncLoadLevel.LoadScene("XintanaLegendsGo_Menu");
+            }
+            else if (currentScene.name.Contains("credits"))
+            {
+                AsyncLoadLevel.LoadScene("SettingsScreen");
+            }
+            else if (currentScene.name.Contains("Pokedex"))
+            {
+                AsyncLoadLevel.LoadScene("XintanaLegendsGo_Menu");
+            }
+        }
         
 
 	}
