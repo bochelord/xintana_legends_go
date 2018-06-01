ea201601660effba6b19b41278d50324c7ee000f
Manuel Gonzalez <leunam.san@gmail.com>
2018-06-01 16:55:48 +0200

    [Scene+Code] - Amazon leaderboards implemented. Ready to test.

---
+++ Assets/Xintana Volcanic/Scripts/RadAmazonLeaderboard.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadAmazonLeaderboard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if(AGSClient.IsServiceReady()) {
+
+            AGSLeaderboardsClient.SubmitScoreSucceededEvent += submitScoreSucceeded;
+            AGSLeaderboardsClient.SubmitScoreFailedEvent += submitScoreFailed;
+
+        }
+    }
+
+    public void ShowAmazonLeaderboard()
+    {
+        AGSLeaderboardsClient.ShowLeaderboardsOverlay();
+    }
+
+    public void SubmitScoreToAmazon(int score)
+    {
+        AGSLeaderboardsClient.SubmitScore("leaderboard_high_score", score);
+    }
+
+    private void submitScoreSucceeded(string leaderboardId)
+    {
+        Debug.Log("Score has been submitted successfully to " + leaderboardId);
+    }
+
+    private void submitScoreFailed(string leaderboardId, string error)
+    {
+        Debug.Log("Score has not been submitted to " + leaderboardId);
+    }
+
+}
